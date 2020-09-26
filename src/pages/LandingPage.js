3a4bca1d2dfe9a3d582a18a028d13a5ec0ae391d
IanCode09 <ianlombu@protonmail.com>
2020-09-26 23:59:10 +0700

    fix misstyped class + complete component parts/footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,16 +4,14 @@ import Header from 'parts/Header'
 import Hero from 'parts/Hero'
 import MostPicked from 'parts/MostPicked'
 import Categories from 'parts/Categories'
+import Testimony from 'parts/Testimony'
+import Footer from 'parts/Footer'
 
 import landingPage from 'json/landingPage.json'
 
 export default class LandingPage extends Component {
 
-    constructor(props) {
-        super(props)
-        this.refMostPicked = React.createRef();
-    }
-
+    
     render () {
         return (
             <> 
@@ -24,6 +22,10 @@ export default class LandingPage extends Component {
                 <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
 
                 <Categories data={landingPage.categories} />
+
+                <Testimony data={landingPage.testimonial} />
+
+                <Footer />
             </>
         )
     }
--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Button from 'elements/Button'
 
 export default function MostPicked(props) {
-    return (
+    return (        
         <section className="container" ref={props.refMostPicked}>
             <h4 className="mb-3">Most Picked</h4>
             <div className="container-grid">
@@ -21,7 +21,7 @@ export default function MostPicked(props) {
                                     <img src={item.imageUrl} alt={item.name} className="img-cover"></img>
                                 </figure>
                                 <div className="meta-wrapper">
-                                    <Button type="link" className="streched-link d-block text-white" href={`/properties/${item._id}`}>
+                                    <Button type="link" className="stretched-link d-block text-white" href={`/properties/${item._id}`}>
                                         <h5>{item.name}</h5>
                                     </Button>
                                     <span>
